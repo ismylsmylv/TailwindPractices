9540ad2405f17f7ee73dee21ef5591f84fae22a8
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-04-12 21:31:27 +0400

    navbar style updated

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,17 +1,34 @@
 import React from "react";
-
+import { IoMdContact } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoMdNotifications } from "react-icons/io";
+import { MdAccountCircle } from "react-icons/md";
 type Props = {};
 
 function Navbar({}: Props) {
   return (
-    <div>
-      <div className="logo">Shoplead</div>
-      <div className="navs">
-        <div className="nav">home</div>
-        <div className="nav">shop</div>
-        <div className="nav">pages</div>
-        <div className="nav">blog</div>
-        <div className="nav">about</div>
+    <div className="bg-red w-full shadow-md flex place-content-around p-10 items-center	">
+      <div className="logo font-Dancing text-5xl font-black">Shoplead</div>
+      <div className="navs flex  place-content-around items-center	">
+        <div className="nav px-5 capitalize">home</div>
+        <div className="nav px-5 capitalize">shop</div>
+        <div className="nav px-5 capitalize">pages</div>
+        <div className="nav px-5 capitalize">blog</div>
+        <div className="nav px-5 capitalize">about</div>
+      </div>
+      <div className="icons flex place-content-around items-center	">
+        <div className="icon px-5">
+          <IoMdContact size={25} />
+        </div>
+        <div className="icon px-5">
+          <FaShoppingCart size={25} />
+        </div>
+        <div className="icon px-5">
+          <IoMdNotifications size={25} />
+        </div>
+        <div className="icon px-5">
+          <MdAccountCircle size={25} />
+        </div>
       </div>
     </div>
   );
