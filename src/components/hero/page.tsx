943771ff3ee4e28c5ea7a2d2d3d96a23975280f0
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-04-13 21:37:28 +0400

    hero style updates

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -4,24 +4,31 @@ type Props = {};
 
 function Hero({}: Props) {
   return (
-    <div className="Hero flex content-between">
-      <div className="left">
-        <div className="heading text-8xl uppercase font-black">
-          perfect time to shop
-        </div>
-        <button>more collection</button>
-        <div className="statics">
-          <div className="kcount">
-            <div className="upper">10k</div>
-            <div className="lower">happy customers</div>
+    <div className="container mx-auto ">
+      <div className="Hero flex content-between container my-auto mx-0 p-20">
+        <div className="left w-1/2">
+          <div className="heading text-8xl text-left uppercase font-black ">
+            perfect time to shop
           </div>
-          <div className="pcount">
-            <div className="upper">400+</div>
-            <div className="lower">daily sales</div>
+          <button className="rounded-md px-10 py-5 mt-10 bg-purple-700 capitalize text-white">
+            more collection
+          </button>
+          <div className="stats flex py-5 mt-10 content-center justify-items-center">
+            <div className="kcount flex flex-col content-center justify-items-center">
+              <div className="upper text-4xl font-bold">10k</div>
+              <div className="lower text-gray-500 capitalize">
+                happy customers
+              </div>
+            </div>
+            <div className="line mx-10  w-1 h-13 bg-gray-500"></div>
+            <div className="pcount flex flex-col content-center justify-items-center">
+              <div className="upper text-4xl font-bold">400+</div>
+              <div className="lower text-gray-500 capitalize">daily sales</div>
+            </div>
           </div>
         </div>
+        <div className="right">sdfsdf</div>
       </div>
-      <div className="right">sdfsdf</div>
     </div>
   );
 }
